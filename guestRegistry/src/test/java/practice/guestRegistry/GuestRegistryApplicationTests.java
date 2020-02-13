33bca9a0e83b9f8b3c2ec0c0a00862dfb992ad9f
Dainius <dainius133@gmail.com>
2020-02-13 22:38:48 +0200

    New structure and example

---
+++ guestRegistry/src/test/java/practice/guestRegistry/GuestRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.guestRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
