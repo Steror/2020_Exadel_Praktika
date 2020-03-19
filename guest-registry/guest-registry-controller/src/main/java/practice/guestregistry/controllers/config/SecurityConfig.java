412c08ad3bf8df65f99983cc058c94627a121ef2
Steror <tomas.kizielas@gmail.com>
2020-03-20 00:18:50 +0200

    Added back authorization

---
+++ guest-registry/guest-registry-controller/src/main/java/practice/guestregistry/controllers/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package practice.guestregistry.controllers.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+//@Order(SecurityProperties.BASIC_AUTH_ORDER)    // ACCESS_OVERRIDE_ORDER undefined, probably old spring
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true)
+//        securedEnabled = true,
+//        jsr250Enabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/*").hasRole("USER")
+                .antMatchers("/login", "/user").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("pass")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
---
+++ guest-registry/guest-registry-controller/src/main/java/practice/guestregistry/controllers/config/User.java
@@ -0,0 +1,9 @@
+package practice.guestregistry.controllers.config;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String userName;
+    private String password;
+}
