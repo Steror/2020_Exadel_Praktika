63289a5b06b2c7b40c3e7f31e34ad0ff8ea5df09
Steror <tomas.kizielas@gmail.com>
2020-03-27 15:05:20 +0200

    Delete unused SequenceID

--- guest-registry/guest-registry-data/guest-registry-data-mongo-impl/src/main/java/practice/guestregistry/data/mongo/daoimpl/SequenceDao.java
+++
@@ -1,8 +0,0 @@
-//package practice.guestregistry.dao;
-//
-//import practice.guestregistry.exceptions.SequenceException;
-//
-//public interface SequenceDao {
-//    long getNextSequenceId (String key) throws SequenceException;
-//    void initCollection (String tableName);
-//}
